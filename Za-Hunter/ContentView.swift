834f7f0cbb1a0d60a5824ee6c8a0c6b56ad6dd80
Lena Rotteveel <lenarotteveel@gmail.com>
2021-05-05 14:33:04 -0500

    Added pizza icon and link to store website

--- Za-Hunter/ContentView.swift
+++ Za-Hunter/ContentView.swift
@@ -10,47 +10,49 @@ import MapKit
 
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(
-           center: CLLocationCoordinate2D(
-               latitude: 42.15704,
-               longitude: -88.14812),
-           span: MKCoordinateSpan(
-               latitudeDelta: 0.05,
-               longitudeDelta: 0.05)
-       )
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
     @StateObject var locationManager = LocationManager()
-        @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var places = [Place]()
     
     var body: some View {
         Map(
-                   coordinateRegion: $region,
-                   interactionModes: .all,
-                   showsUserLocation: true,
-                   userTrackingMode: $userTrackingMode,
-                   annotationItems: places) { place in
-                   MapPin(coordinate: place.annotation.coordinate)
-               }
-               .onAppear(perform: {
-                   performSearch(item: "Pizza")
-               })
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.annotation.coordinate) {
+                Marker(mapItem: place.mapItem)
+            }
+        }
+        .onAppear(perform: {
+            performSearch(item: "Pizza")
+        })
     }
     
     func performSearch(item: String) {
         let searchRequest = MKLocalSearch.Request()
-               searchRequest.naturalLanguageQuery = item
-               searchRequest.region = region
-               let search = MKLocalSearch(request: searchRequest)
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
         search.start { (response, error) in
             if let response = response {
                 for mapItem in response.mapItems {
-                                    let annotation = MKPointAnnotation()
-                                    annotation.coordinate = mapItem.placemark.coordinate
-                                    annotation.title = mapItem.name
-                                    places.append(Place(annotation: annotation, mapItem: mapItem))
-                                }
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
             }
         }
-        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -63,4 +65,15 @@ struct Place: Identifiable {
     let id = UUID()
     let annotation: MKPointAnnotation
     let mapItem: MKMapItem
+}
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("pizza")
+            })
+        }
+    }
 }
